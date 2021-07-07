d410ba568337441f0b39490ac268d686df32b460
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2021-07-06 21:21:37 -0700

    Added gitignore and setup for calc.py

---
+++ calc/app.py
@@ -0,0 +1,12 @@
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+# Put your app in here.
+def sample_code():
+    return "Hello!"
